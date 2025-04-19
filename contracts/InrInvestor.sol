f1e1c775aa89513bfcdeac760dd904d3dd930ff0
Maxcoin Developer <maxcoindev@gmail.com>
2025-04-19 20:13:09 +0530

    payments in inr

--- contracts/InrInvestor.sol
+++ contracts/InrInvestor.sol
@@ -3,46 +3,68 @@
 pragma solidity ^0.8.22;
 
 
-import {Investor} from "./Investor.sol";
+import {AbstractInvestor, Investment} from "./AbstractInvestor.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
 
 import "hardhat/console.sol";
 
 
-event Open(address to, uint256 amount1, uint256 amount2, uint256 inr, bytes32 hash);
-event Close(uint256 tokenId, address to, uint256 amount1, uint256 amount2, uint256 total,  uint256 inr);
+event Open(address to, uint256 amountA, uint256 amountB, uint256 inr, bytes32 hash);
+event Close(uint256 tokenId, address to, uint256 amountA, uint256 amountB, uint256 total, uint256 inr_rate);
 event DevFee(uint256 fee);
 event Profit(uint256 max, uint256 per_day);
 event WithdrawProfit(address to, uint256 amount);
-// ToDo: Add investment hash
+// ToDo: Profit in INR
+// ToDo: [x] INR exchange rate
+// ToDo: [x] Add Exchanger group
+// ToDo: [x] Allow withdraw only to exchanger
+// ToDo: [x] Add manager to autoapproval of account
 
 
-contract InrInvestor is Investor {
+
+contract InrInvestor is AbstractInvestor {
     using SafeERC20 for IERC20;
  
     bytes32 public constant TRADER_ROLE = keccak256("TRADER_ROLE");
     bytes32 public constant DEV_ROLE = keccak256("DEV_ROLE");
     bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
     bytes32 public constant ACCOUNTER_ROLE = keccak256("ACCOUNTER_ROLE");
+    bytes32 public constant EXCHANGER_ROLE = keccak256("EXCHANGER_ROLE");
+
     uint256 public dev_fee ;
     uint256 public dev_balance;
+    uint256 public inr_rate;
     mapping(uint256 _tokenId => uint256 _inr) investedInr;
     mapping(bytes32 => bool) exists;
     mapping(bytes32 => uint256) hash2tokenId;
 
+    // Counters
+    uint256 totalInvestmentA; //How much it was invested total
+    uint256 totalPaidA; // How much it was paid in total
+    uint256 totalProfitA; // How much it was made in total
+    uint256 totalDeficiteA;
 
-    constructor (address _tokenA, address _tokenB, address _V2router) Investor( _msgSender(),  _tokenA,  _tokenB,  _V2router) {
+
+
+    constructor (address _tokenA, address _tokenB, address _V2router) AbstractInvestor( _msgSender(),  _tokenA,  _tokenB,  _V2router) {
         _grantRole(TRADER_ROLE, _msgSender());
         _grantRole(MANAGER_ROLE, _msgSender());
         _grantRole(ACCOUNTER_ROLE, _msgSender());
         dev_fee = 1;
-        _updateProfit(8000, 5);
+        inr_rate = 1;
+        _updateDefaultProfitRate(8000, 5);
+    }
 
+    function setInrRate(uint256 rate) public onlyRole(ACCOUNTER_ROLE) {
+        inr_rate = rate;
     }
 
     function updateProfit(uint256 max, uint256 per_day) public onlyRole(MANAGER_ROLE) {
-        updateProfit(max, per_day);
+        _updateDefaultProfitRate(max, per_day);
     }
 
     function updateDevFee( uint256 _fee) public onlyRole(DEFAULT_ADMIN_ROLE){
@@ -54,38 +76,48 @@ contract InrInvestor is Investor {
         _update_profit_rate(tokenId, _max_profit, _profit_per_day); 
     }
 
-    function withdrawProfit(address to) public onlyRole(ACCOUNTER_ROLE) {
-        uint256 amount = IERC20(tokenA).balanceOf(address(this)); 
-        require(amount > dev_balance, "Not enough money");
-        amount -= dev_balance; 
-        IERC20(tokenA).safeTransfer(to,amount);
+    function withdrawProfit(address to, uint256 amount) public onlyRole(ACCOUNTER_ROLE) {
+        uint256 balance = IERC20(tokenA).balanceOf(address(this));
+        require(amount <= (balance - dev_balance), "Not enough money"); 
+        IERC20(tokenA).safeTransfer(to, amount);
         emit WithdrawProfit(to, amount);
     }
 
 
-    function openTrade(address to, uint256 amountA, uint256 amountBOutMin, uint deadline, uint256 inr, bytes32 _hash) onlyRole(TRADER_ROLE) external payable returns(uint[] memory amounts) {
+    function openTrade(address to, uint256 amountBOutMin, uint deadline, uint256 inr, bytes32 _hash) onlyRole(TRADER_ROLE) external payable returns(uint[] memory amounts) {
         require(!exists[_hash], "hash exists");
+        uint256 amountA = SCALE * inr / inr_rate;
         exists[_hash] = true;
         dev_balance +=  dev_fee * amountA/SCALE;
         uint256 tokenId = _mint(to);
         hash2tokenId[_hash] = tokenId;
+        investedInr[tokenId] = inr;
         amounts = _openTrade(tokenId, amountA, amountBOutMin, deadline);
+        totalInvestmentA += amounts[0];
         _initalInvestment(tokenId, amounts[0], amounts[1], default_profit_max, default_profit_per_day);
         emit Open(to, amounts[0], amounts[1], inr, _hash);
         return amounts;
     }
 
 
-    function closeTrade(uint256 tokenId, uint256 amountOutMin, uint deadline, uint256 inr) onlyRole(TRADER_ROLE) external returns(uint256) {
-        console.log("Before check owner");
-        address to = _ownerOf(tokenId);
-        _isAuthorized(to, _msgSender(), tokenId);
+    function closeTrade(uint256 tokenId, address to, uint256 amountOutMin, uint deadline) onlyRole(TRADER_ROLE) external returns(uint256) {
+        require(hasRole(EXCHANGER_ROLE, to), "Reciever is not exchanger");
         console.log("Before close trade");
-        (uint256 amountA,uint256 amountB, uint256 total)  = _closeTrade(tokenId, to, amountOutMin, deadline);
-        console.log("After close trade");
+        (uint256 amountA, uint256 amountB)  = _closeTrade(tokenId, amountOutMin, deadline);
         dev_balance +=  dev_fee * amountA/SCALE;
-         _update(address(0), tokenId, _msgSender()); // It checks authentication
-        emit Close(tokenId, to,  amountA, amountB, total, inr);
+        uint256 total = _profit(amountA, tokenId);
+
+        if (amountA > total) {
+            totalProfitA += amountA - total;
+        } else { 
+            if (amountA <  total) {
+                totalDeficiteA += total - amountA;
+            }
+        }
+        totalPaidA += total;
+        IERC20(tokenA).safeTransfer(to, total);
+         _update(address(0), tokenId, _msgSender()); // Burn, It checks authentication
+         emit Close(tokenId, to,  amountA, amountB, total, inr_rate);
         return total;
     }
     
@@ -95,9 +127,25 @@ contract InrInvestor is Investor {
     }
 
 
+    function isApprovedForAll(address owner, address operator) public view override(ERC721, IERC721) virtual returns (bool) {
+        return super.isApprovedForAll(owner,operator) || hasRole(MANAGER_ROLE, operator);
+    }
+
+
     // internal 
-    function _msgValue() internal view virtual returns (uint256) {
-        return msg.value ;
+    function _profit(uint256 amount, uint256 tokenId) internal override view returns(uint256 total) {
+        Investment storage investment = investments[tokenId];
+        total = investedInr[tokenId] * SCALE / inr_rate;
+        if (amount > total) {
+            uint48 time_range = _daysFrom(investment.start);
+            uint256 profit = amount - total;
+            if (time_range <= investment.max_profit/investment.profit_per_day ) {
+                total += profit * time_range * investment.profit_per_day/SCALE;
+            } else {
+                total += profit * investment.max_profit/SCALE;
+            }
+        }
+        return total;
     }
 
 
--- contracts/Investor.sol
+++ contracts/Investor.sol
@@ -1,200 +1,84 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.22;
-import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import {ERC721Pausable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IUniswapV2Router02} from "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
-import {IUniswapV2Factory} from "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 
-import "hardhat/console.sol";
+import {AbstractInvestor, Investment} from "./AbstractInvestor.sol";
 
-
-struct Investment {
-    uint48 start;
-    uint256 investmentA; // Initial inventment amount
-    uint256 amountB; // Balance of token B
-    uint256 max_profit; // 1%=100, when SCALE=1000
-    uint256 profit_per_day; // 0.05% = 5, when SCALE=1000
-}
+event Open(address to, uint256 amount1, uint256 amount2);
+event Close(uint256 tokenId, address to, uint256 amount1, uint256 amount2, uint256 total);
 
 
-// Contract, which allows to track investments.
-// Each investment represented as NFT
-contract Investor is ERC721, ERC721Enumerable, ERC721Pausable, AccessControl{
 
-    event Invest(uint256 tokenId,uint256 amount, address manager, address reciever);
-    event ProfitRate(uint256 tokenId, uint256 max_profit, uint256 profit_per_day);
-    event Profit(uint256 max, uint256 per_day);
-
+contract Investor is AbstractInvestor {
     using SafeERC20 for IERC20;
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-    uint256 constant SCALE = 10000;
-    uint256  default_profit_max;
-    uint256  default_profit_per_day;
-
-    uint256 private _nextTokenId;
-    address public tokenA;
-    address public tokenB;
-    address public uniswapV2Router02;
-
-    mapping (uint256 => Investment) public investments;
 
     uint256 totalInvestmentA; //How much it was invested total
     uint256 totalPaidA; // How much it was paid in total
     uint256 totalProfitA; // How much it was made in total
-    uint256 totalBalanceB;
-
-    constructor(address defaultAdmin, address _tokenA, address _tokenB, address _V2router)
-        ERC721("NftInvest", "NI")
-    {
-        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
-        _grantRole(PAUSER_ROLE, defaultAdmin);
-        tokenA = _tokenA;
-        tokenB = _tokenB;
-        uniswapV2Router02 = _V2router;
-        IERC20(_tokenA).approve(_V2router, type(uint256).max);
-        IERC20(_tokenB).approve(_V2router, type(uint256).max);
-    }
-
-    // Internal functions
-    function _updateProfit(uint256 _max, uint256 _per_day) internal {
-        default_profit_max = _max;
-        default_profit_per_day = _per_day;
-        emit Profit(_max, _per_day);
-    }
+    uint256 totalDeficiteA;
 
 
-    function _mint(address to) internal returns (uint256) {
-        uint256 tokenId = _nextTokenId++;
-        _safeMint(to, tokenId);
-        return tokenId;
-    }
+    constructor (address _tokenA, address _tokenB, address _V2router, address _factory) 
+        AbstractInvestor( _msgSender(),  _tokenA,  _tokenB,  _V2router) {}
 
-    function _update_profit_rate(uint256 tokenId, uint256 _max_profit, uint256 _profit_per_day) internal {
-        require(_ownerOf(tokenId) != address(0), "Investor: Token not exists");
-        Investment storage investment = investments[tokenId];
-        investment.max_profit = _max_profit;
-        investment.profit_per_day = _profit_per_day;
-        emit ProfitRate(tokenId, _max_profit, _profit_per_day);
-    }
-
-    function _initalInvestment(uint256 tokenId, uint256 amountA, uint256 amountB, uint256 _max_profit, uint256 _profit_per_day) internal {
-        investments[tokenId] = Investment({
-            investmentA: amountA,
-            amountB: amountB,
-            start: clock(),
-            max_profit: _max_profit,
-            profit_per_day: _profit_per_day
-        });
-        totalInvestmentA += amountA;
-    }
-
-    // returns: amountA, amountB, total payed A 
-    function _closeTrade(uint256 tokenId, address to, uint256 amountOutMin, uint deadline) internal returns(uint256,uint256,uint256) {
-        Investment storage investment = investments[tokenId];
-        uint256 amount = investment.amountB;
-        address[] memory path = new address[](2);
-        path[0] = tokenB;
-        path[1] = tokenA;
-        uint256[] memory amounts = IUniswapV2Router02(uniswapV2Router02).swapExactTokensForTokens(
-            amount,
-            amountOutMin,
-            path,
-            address(this),
-            deadline
-        );
-
-        uint256 total = _profit(amounts[1], investment);
-        console.log("Close trade, total, investmentA:", total , investment.investmentA );
-        if (investment.investmentA >= total) {
-            console.log("Close trade, deficite:", total ,  investment.investmentA);
-            total = investment.investmentA;
-        } else {
-             totalProfitA += amounts[1] - total;
-        }
-        console.log("to, total",to, total);
-        IERC20(tokenA).safeTransfer(to, total);
-        investment.amountB -= amounts[0];
-        return (amounts[1], amounts[0], total);
-    }
-
-
-
-    function _openTrade(uint256 tokenId, uint256 amount, uint256 amountOutMin, uint deadline) internal returns(uint[] memory amounts) {
-                address[] memory path = new address[](2);
-        IERC20(tokenA).safeTransferFrom(_msgSender(),address(this),amount);
-        path[0] = tokenA;
-        path[1] = tokenB;
-        amounts = IUniswapV2Router02(uniswapV2Router02).swapExactTokensForTokens(
-            amount,
-            amountOutMin,
-            path,
-            address(this),
-            deadline
-        );
-        investments[tokenId].amountB += amounts[1];
+    function openTrade(uint256 amount, uint256 amountBOutMin, uint deadline)  external returns(uint[] memory amounts) {
+        address to = _msgSender();
+        uint256 tokenId = _mint(to);
+        amounts = _openTrade(tokenId, amount, amountBOutMin, deadline);
+        _initalInvestment(tokenId, amounts[0], amounts[1], default_profit_max, default_profit_per_day);
+        totalInvestmentA += amounts[0];
+        emit Open(to, amounts[0], amounts[1]);
         return amounts;
     }
 
-    function _profit(uint256 amount, Investment storage investment) internal view returns(uint256 total) {
-        total = investment.investmentA;
-        if (amount > total) {
-            uint48 time_range = _daysFrom(investment.start);
-            uint256 profit = amount - investment.investmentA;
-            if (time_range <= investment.max_profit/investment.profit_per_day ) {
-                total += profit * time_range * investment.profit_per_day/SCALE;
-            } else {
-                total += profit * investment.max_profit/SCALE;
+    function closeTrade(uint256 tokenId, uint256 amountOutMin, uint deadline)  external returns(uint256) {
+        address to = _ownerOf(tokenId);
+        require(to != address(0), "Investor: Token not exists");
+        (uint256 amountA,uint256 amountB)  = _closeTrade(tokenId, amountOutMin, deadline);
+
+        uint256 total = _profit(amountA, tokenId);
+
+        if (amountA > total) {
+            totalProfitA += amountA - total;
+        } else { 
+            if (amountA <  total) {
+                totalDeficiteA += total - amountA;
             }
         }
+        totalPaidA += total;
+        IERC20(tokenA).safeTransfer(to, total);
+         _update(address(0), tokenId, _msgSender()); // Burn, It checks authentication
+        emit Close(tokenId, to, amountA, amountB, total);
         return total;
     }
 
-    // return full days from some start point
-    // start - start time in seconds
-    function _daysFrom(uint48 start) internal view returns(uint48) {
-        require(clock() >= start, "invalid time range");
-        return( (clock() - start) / 1 days);
-    }
-
-    // service functions
-    function clock() public view  returns (uint48) {
-        return uint48(block.timestamp);
-    }
-
-    // solhint-disable-next-line func-name-mixedcase
-    function CLOCK_MODE() public pure returns (string memory) {
-        return "mode=timestamp";
+    // returns AmountB, AmountA, totalA
+    function viewPendingProfit(uint256 tokenId) public view returns(uint[] memory amounts) {
+        assert(_ownerOf(tokenId) != address(0));
+        address[] memory path = new address[](2);
+        path[0] = tokenB;
+        path[1] = tokenA;
+        Investment storage inv = investments[tokenId];
+        IUniswapV2Router02(uniswapV2Router02).getAmountsOut(inv.amountB, path);
+        uint256 total = _profit(amounts[1], tokenId);
+        amounts[2]=total;
+        return amounts;
     }
 
 
-    // The following functions are overrides required by Solidity.
-    function _update(address to, uint256 tokenId, address auth)
-        internal
-        override(ERC721, ERC721Enumerable, ERC721Pausable)
-        returns (address)
-    {
-        return super._update(to, tokenId, auth);
+    // Add some extra protections
+    function withdrawStuckETH(address payable _addressTo, uint256 _amount) onlyRole(DEFAULT_ADMIN_ROLE) public returns(uint256){
+        _addressTo.transfer(_amount);
+        return address(this).balance;
     }
 
-    function _increaseBalance(address account, uint128 value)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
-        super._increaseBalance(account, value);
+    function withdrawStuckErc20Tokens(address _token, address _to) onlyRole(DEFAULT_ADMIN_ROLE) public {
+        uint256 tokenAmount = IERC20(_token).balanceOf(address(this));
+        IERC20(_token).transfer(_to, tokenAmount);
     }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable, AccessControl)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
 
 }
