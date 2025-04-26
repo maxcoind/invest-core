afc155bb3a00208afc6c3363ac55425f360a4041
Maxcoin Developer <maxcoindev@gmail.com>
2025-04-26 15:03:13 +0530

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,21 +6,9 @@ import "@nomicfoundation/hardhat-foundry";
 const config: HardhatUserConfig = {
   
   networks: {
-    hardhat: {
-      chainId: 1337,
-      forking: {
-        url: "https://eth-mainnet.g.alchemy.com/v2/your-alchemy-key",
-        blockNumber: 12345678,
-      },
-    },
-    goerli: {
-      url: "https://eth-goerli.g.alchemy.com/v2/your-alchemy-key",
-      accounts: [vars.get("PRIVATE_KEY")],
-    },
     mainnet: {
       url: "https://bsc-dataseed.binance.org/",
       chainId: 56,
-      gasPrice: 20000000000,
       accounts: [vars.get("PRIVATE_KEY")],
     }
   
